07b015c9db5d672d72cd1ff4ba4bdf2dce34cf27
Whashi <whashi88@gmail.com>
2025-04-28 15:36:07 -0700

    Remove server proxy configuration and adjust build options in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://www.bungie.net/Platform",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        console.log("Environment variables:", process.env);
+        warn(warning);
       },
     },
   },
